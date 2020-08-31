883c84e0e95bbe63d7e25ba76c96463f88caeef9
futuredev <futuredev1001@yandex.com>
2020-08-31 17:52:09 +0200

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^zqqr#_0z^94y7@c75a*l2m-z3+6s=3r0*ro!yo8c-=hixz(=0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
