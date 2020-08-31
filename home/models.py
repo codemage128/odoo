09eed2a3c566096ad99225f614bcbaec5da4dab7
futuredev <futuredev1001@yandex.com>
2020-08-31 07:42:18 +0200

    update

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Template(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=300)
+    category = models.CharField(max_length=100)
--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.views import View
+from home.models import Template
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
@@ -12,9 +17,65 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
 
-class ReportView(TemplateView):
+class ReportView(View):
     template_name = 'reports.html'
+    # contact List
+    addressList = []
+    telList = []
+    cityList = []
+    occupationList = []
 
+    # task List
+    taskCategoryList = []
+    taskOwnerList = []
+
+    # staff List
+    phoneList = []
+    staff_addressList = []
+    response = {}
+
+    def __init__(self):
+        self.addressList = []
+        self.telList = []
+        self.cityList = []
+        self.occupationList = []
+        self.taskCategoryList = []
+        self.taskOwnerList = []
+        self.phoneList = []
+        self.staff_addressList = []
+
+    def loadData(self):
+        templateList = Template.objects.all()
+        for template in templateList:
+            if template.category == "addressList": self.addressList.append(template)
+            if template.category == "telList": self.telList.append(template)
+            if template.category == "cityList": self.cityList.append(template)
+            if template.category == "occupationList": self.occupationList.append(template)
+            if template.category == "taskCategoryList": self.taskCategoryList.append(template)
+            if template.category == "taskOwnerList": self.taskOwnerList.append(template)
+            if template.category == "phoneList": self.phoneList.append(template)
+            if template.category == "staff_addressList": self.staff_addressList.append(template)
+        self.response = {"addressList": self.addressList, "telList": self.telList, "cityList": self.cityList,
+                         "occupationList": self.occupationList, "taskCategoryList": self.taskCategoryList,
+                         "taskOwnerList": self.taskOwnerList, "phoneList": self.phoneList,
+                         "staff_addressList": self.staff_addressList}
+
+    def get(self, request):
+        self.loadData()
+        return render(request, self.template_name, {"data": self.response})
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        category = request.POST.get('category')
+        message = False
+        try:
+            Template.objects.create(name=name, category=category)
+            self.loadData()
+            message = True
+        except:
+            message = False
+        return render(request, self.template_name, {"data": self.response, "message": message})
+        # return redirect('report')
 
 class EditorView(TemplateView):
     template_name = 'edit.html'
